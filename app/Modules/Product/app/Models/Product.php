9a03fd55d4e2d0ecedd8ae4db6cf092f1177a02c
ARTALAX <urazmakin135135@gmail.com>
2026-03-01 20:56:08 +0300

    with php doc

--- app/Modules/Product/app/Models/Product.php
+++ app/Modules/Product/app/Models/Product.php
@@ -8,6 +8,29 @@ use Modules\Product\Database\Factories\ProductFactory;
 
 // use Modules\Product\Database\Factories\ProductFactory;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property float $price
+ * @property float|null $weight
+ * @property string|null $category
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Modules\Product\Database\Factories\ProductFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereWeight($value)
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
     use HasFactory;
